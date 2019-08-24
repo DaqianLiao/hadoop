fee273447552cce12d0e2bc1b2ca9940e410ef82
diligent_leo <360989637@qq.com>
2019-08-24 18:03:51 +0800

    add flink helloWorld

---
+++ flink-learning/src/main/java/com/ldq/study/HelloWorld.java
@@ -0,0 +1,48 @@
+package com.ldq.study;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.DataSet;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.operators.AggregateOperator;
+import org.apache.flink.api.java.operators.DataSource;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.util.Collector;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class HelloWorld {
+    public static void main(String[] args) throws Exception {
+        //创建运行环境
+        ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+
+        List<String> list = Arrays.asList(new String[]{"hello", "java", "hello", "flink"});
+        //将集合转化为数据源
+        DataSet<String> text = env.fromCollection(list);
+        //打印数据结果
+//        stringDataSource.print();
+
+        DataSet<Tuple2<String, Integer>> counts = text.flatMap(new LineSplitter())
+                .groupBy(0)
+                .sum(1);
+
+        counts.print();
+        System.out.println(counts.collect().toString());
+    }
+
+    public static class LineSplitter implements FlatMapFunction<String, Tuple2<String, Integer>> {
+
+        @Override
+        public void flatMap(String s, Collector<Tuple2<String, Integer>> collector) {
+            System.out.println("s = " + s);
+            String[] split = s.split("\\W+");
+            for (String s1 : split) {
+                System.out.println("s1 = " + s1);
+                if (s1.length() > 0) {
+                    collector.collect(new Tuple2<String, Integer>(s1, 1));
+                }
+            }
+        }
+    }
+
+}
